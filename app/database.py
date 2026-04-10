0dfde3d0a4d5c00eb1bfac2ab784dfdbe1875870
NikLev <kapusta.pv@gmail.com>
2026-04-10 15:28:05 +0300

    Изменение database.py перед пушем на VPS

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://demo_user:demo_pass@localhost:5432/demo_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://demo_user:demo_pass@localhost:5432/demo_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
